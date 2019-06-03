5bbf1ad03db22ceda70c609717a6c0329bb6b9b8
Clemens Kaposi <clemens@kaposi.name>
2019-06-03 21:28:11 +0200

    Add tests for `ReprMixin`

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/tests/test_db.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/tests/test_db.py
@@ -1,9 +1,11 @@
 """Tests for the ``db`` module."""
 
+import re
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from ..db import db, migrate
+from ..db import db, migrate, ReprMixin
 
 
 def test_db_is_flask_sqlalchemy_instance():
@@ -14,3 +16,34 @@ def test_db_is_flask_sqlalchemy_instance():
 def test_migrate_is_flask_migrate_instance():
     """Verify that :data:`migrate` is a :class:`Migrate` instance."""
     assert isinstance(migrate, Migrate)
+
+
+class MyReprModel(ReprMixin):
+    """Dummy class for testing the :class:`ReprMixin`."""
+
+
+class TestReprMixin:
+    """Tests for the :class:`ReprMixin` class."""
+
+    def test_object_with_id(self):
+        """Check the representation string of an object with an ID."""
+        my_object = MyReprModel()
+        my_object.id = 49
+        assert repr(my_object) == "<MyReprModel #49>"
+
+    def test_object_without_id(self):
+        """Check the representation string of an object without an ID."""
+        my_object = MyReprModel()
+        assert re.match(r"^<MyReprModel 0x[0-9a-f]+>", repr(my_object))
+
+    def test_object_with_none_id(self):
+        """Check the representation string of an object with a ``None`` ID."""
+        my_object = MyReprModel()
+        my_object.id = None
+        assert re.match(r"^<MyReprModel 0x[0-9a-f]+>", repr(my_object))
+
+    def test_object_with_empty_id(self):
+        """Check the representation string of an object with an empty ID."""
+        my_object = MyReprModel()
+        my_object.id = ""
+        assert re.match(r"^<MyReprModel 0x[0-9a-f]+>", repr(my_object))
