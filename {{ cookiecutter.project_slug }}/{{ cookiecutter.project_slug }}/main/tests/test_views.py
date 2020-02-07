5a60a4b4f3f0104a1d5bc35ea092fd34f9a2240e
Clemens Kaposi <clemens.kaposi@has-to-be.com>
2020-02-07 07:43:34 +0100

    Add basic health check view to main blueprint

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main/tests/test_views.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main/tests/test_views.py
@@ -1,5 +1,7 @@
 """Tests for the ``main`` blueprint’s views."""
 
+from http import HTTPStatus
+
 from ...tests.base import TestCaseBase
 
 
@@ -12,3 +14,31 @@ class TestIndexView(TestCaseBase):
         message = response.get_data().decode()
         assert response.mimetype == "text/plain"
         assert "{{ cookiecutter.project_title }}" in message
+
+
+class TestHealthcheck(TestCaseBase):
+    """Tests for the healthcheck view."""
+
+    def get_default_response(self):
+        """Call the view without parameters, and return the response.
+
+        :rtype: flask.Flask.response_class
+        """
+        response = self.client.get("/.well-known/healthcheck")
+        return response
+
+    def test_json_content_type(self):
+        """Verify that the response uses the JSON content type."""
+        response = self.get_default_response()
+        assert response.mimetype == "application/json"
+
+    def test_http_status_code_ok(self):
+        """Check that the response is served with HTTP status “OK”."""
+        response = self.get_default_response()
+        assert response.status_code == HTTPStatus.OK
+
+    def test_response_contains_status(self):
+        """Verify that the response contains status information."""
+        response = self.get_default_response()
+        data = response.get_json()
+        assert data["status"] == "OK"
